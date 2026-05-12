5de089c1d31273652a06b9d67a288c6078e60b44
belaofabriciosiss <belaofabriciosiss@gmail.com>
2026-05-12 13:02:11 -0300

    Add files via upload

---
+++ src/constants/emissores.js
@@ -0,0 +1,13 @@
+/**
+ * Lista de emissores disponíveis para seleção na tela de Gerar APAC.
+ *
+ * Para adicionar um novo município, basta incluir um novo objeto no array:
+ *   { municipio: 'Nome do Município', codigo: 'MXXXXXXXXX' }
+ *
+ * O campo `codigo` é o valor que será gravado no arquivo APAC (tamanho 10, tipo CHAR).
+ */
+export const EMISSORES = [
+  { municipio: 'Guarulhos',    codigo: 'M351880001' },
+  { municipio: 'Santo André',  codigo: 'M352940001' },
+  { municipio: 'Piracicaba',   codigo: 'M353870001' },
+]
