3e33acfaa2393a57b298125855d72adb36030907
belaofabriciosiss <belaofabriciosiss@gmail.com>
2026-05-12 19:57:13 -0300

    Update emissores list with new municipalities

--- src/constants/emissores.js
+++ src/constants/emissores.js
@@ -2,12 +2,13 @@
  * Lista de emissores disponíveis para seleção na tela de Gerar APAC.
  *
  * Para adicionar um novo município, basta incluir um novo objeto no array:
- *   { municipio: 'Nome do Município', codigo: 'MXXXXXXXXX' }
+ *   { municipio: 'Nome do Município', codigo: 'MXXXXXXXXX' } 
  *
  * O campo `codigo` é o valor que será gravado no arquivo APAC (tamanho 10, tipo CHAR).
  */
 export const EMISSORES = [
   { municipio: 'Guarulhos',    codigo: 'M351880001' },
-  { municipio: 'Santo André',  codigo: 'M352940001' },
+  { municipio: 'Mauá',  codigo: 'M352940001' },
   { municipio: 'Piracicaba',   codigo: 'M353870001' },
+  { municipio: 'Santo André', codigo: 'M354780001' },
 ]
